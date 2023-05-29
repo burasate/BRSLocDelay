1ea7ca616b138512f47ef4b3f6ed685b8d162cd8
burasate <61697687+burasate@users.noreply.github.com>
2023-05-29 10:53:59 +0700

    Update support11x.py

--- service/support11x.py
+++ service/support11x.py
@@ -231,7 +231,7 @@ else: #python 2
 #===============================================================================
 
 def add_queue_task(task_name, data_dict):
-    global sys
+    global sys,json
     is_py3 = sys.version[0] == '3'
     if is_py3:
         import urllib.request as uLib
