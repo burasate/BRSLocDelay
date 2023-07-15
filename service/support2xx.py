65d7bcddd46ba60a956296153fe3f14ba9bf96a1
burasate <61697687+burasate@users.noreply.github.com>
2023-07-16 02:58:45 +0700

    Update support2xx.py

--- service/support2xx.py
+++ service/support2xx.py
@@ -34,5 +34,43 @@ def update_version():
 update_version()
 
 '''========================================='''
-# Supporter
+# Queue Task Func
 '''========================================='''
+def add_queue_task(task_name, data_dict):
+    global sys,json
+    is_py3 = sys.version[0] == '3'
+    if is_py3:
+        import urllib.request as uLib
+    else:
+        import urllib as uLib
+
+    if type(data_dict) != type(dict()):
+        return None
+
+    data = {
+        'name': task_name,
+        'data': data_dict
+    }
+    data['data'] = json.dumps(data['data'], indent=4, sort_keys=True)
+    url = 'https://script.google.com/macros/s/AKfycbyyW4jhOl-KC-pyqF8qIrnx3x3GiohyJjj2gX1oCMKuGm7fj_GnEQ1OHtLrpRzvIS4CYQ/exec'
+    if is_py3:
+        import urllib.parse
+        params = urllib.parse.urlencode(data)
+    else:
+        params = uLib.urlencode(data)
+    params = params.encode('ascii')
+    conn = uLib.urlopen(url, params)
+
+'''========================================='''
+# License Key
+'''========================================='''
+def gr_license_script():
+    if sys.version[0] == '3':
+        import urllib.request as uLib
+    else:
+        import urllib as uLib
+    url = 'https://raw.githubusercontent.com/burasate/AniMateAssist/main/service/licsence.py'
+    return uLib.urlopen(url).read()
+exec(gr_license_script())
+self.grl = gr_license(product_name = 'Keyframe Overlap (BRS Locator Delay)', product_code = 'hZBQC')
+self.grl.show_ui()
